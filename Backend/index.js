c90deaace4bf90f872e474882de988c88103fa3c
shaurya2412 <shauryaa2412@gmail.com>
2024-09-21 12:17:02 +0530

    changes done in backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,15 @@
 import express from "express"; // method-2
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+
 dotenv.config({});
 const app = express();
+
+//middleware
+
+// routes
+app.use("/api/v1/user",userRoute); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
